af190360da4dc5b3e5796ce1468717eee70f3c4f
siddhant gulia <67831735+siddcyber@users.noreply.github.com>
2022-07-27 16:54:02 +0530

    added player, bg color

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 # start of first game using pycharm
-#  space invaders ( from youtube )
+#  space invaders ( from YouTube )
 
 import pygame
+
 # initialize pygame
 pygame.init()
 #  create screen display
-screen = pygame.display.set_mode((800,600))
-running = True # value assigned to check if window running
+screen = pygame.display.set_mode((800, 600))
+screen.fill('white')
+running = True  # value assigned to check if window running
 #  Title and focus
 pygame.display.set_caption('Space Invaders')
-icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(pygame.image.load('ufo.png'))
+playerImg = pygame.image.load("player.png")
+playerX = 370
+playerY = 480
+def player():
+    screen.blit(playerImg,(playerX,playerY))
 
 # Game Loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    player()
+    pygame.display.update() # updates the window
