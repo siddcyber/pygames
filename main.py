169d3697a2bbe3352d440b4c70a97639f575af9a
siddhant gulia <67831735+siddcyber@users.noreply.github.com>
2022-07-28 13:29:50 +0530

    added img mobiliy

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import pygame
 pygame.init()
 #  create screen display
 screen = pygame.display.set_mode((800, 600))
-screen.fill('white')
 running = True  # value assigned to check if window running
 #  Title and focus
 pygame.display.set_caption('Space Invaders')
@@ -15,13 +14,30 @@ pygame.display.set_icon(pygame.image.load('ufo.png'))
 playerImg = pygame.image.load("player.png")
 playerX = 370
 playerY = 480
-def player():
-    screen.blit(playerImg,(playerX,playerY))
+playerXChange = 0
+playerYChange = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 # Game Loop
 while running:
+    screen.fill('black')  # the screen fil is set here to update the screen black after every event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    player()
-    pygame.display.update() # updates the window
+        # to check if key was pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerXChange = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerXChange = 0.1
+        # to check if key was released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP:
+                playerXChange = 0
+    playerX += playerXChange
+    player(playerX, playerY)
+    pygame.display.update()  # updates the window
+#
